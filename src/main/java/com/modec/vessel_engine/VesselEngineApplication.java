02ce3c110265d72f3c4b44c7b0e56621ba7ea9a4
Guilherme Simas <guilherme_abinader@hotmail.com>
2020-09-18 20:27:06 -0300

    Initial commit with scaffolding from Spring Initializr

---
+++ src/main/java/com/modec/vessel_engine/VesselEngineApplication.java
@@ -0,0 +1,13 @@
+package com.modec.vessel_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VesselEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VesselEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modec/vessel_engine/VesselEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modec.vessel_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VesselEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
